a3d0fbbdf09316eb123b55e53f1d903a27e8e287
Laura Soares <85892623+laurasoares30@users.noreply.github.com>
2022-05-31 17:03:08 -0300

    Add files via upload

---
+++ Banco.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Banco {
+  
+  public String nome;
+  public List<Conta> conta;
+
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public List<Conta> getContas() {
+    return conta;
+  }
+  public void setContas(List<Conta> contas) {
+    this.conta = contas;
+  }
+
+
+  
+  
+
+  
+}
